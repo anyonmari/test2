451a7d89be3edc2657fcc4a45fdd108b80a48548
Maryna Radasaulevich <m.radasaulevich@astondevs.ru>
2024-05-26 15:31:43 +0300

    added

--- src/test/java/UI/Abstract.java
+++ src/test/java/UI/Abstract.java
@@ -30,11 +30,6 @@ public class Abstract {
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--window-size=1920,1080");
-        options.addArguments("--headless");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--window-size=1920,1080");
         driver = new EventFiringWebDriver(new ChromeDriver(options));
         wait = new WebDriverWait(driver,30);
         actions = new Actions(driver);
